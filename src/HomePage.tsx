b8ca32ba384ba2d3c3ce563f9ba56518a2e40827
Alex Tarnawski <atmarnawski@gmail.com>
2026-01-07 06:22:03 -0600

    move divs

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -8,8 +8,8 @@ export default function HomePage() {
   return (
     <div className="flex justify-center pt-16 pb-4">
       <TableList />
-      <GuestList guests={guests} setGuests={setGuests} />
       <UnassignedList guests={guests} />
+      <GuestList guests={guests} setGuests={setGuests} />
     </div>
   );
 }
