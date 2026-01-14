6f2a33ff1195ad7084ed00055a82f790df1efb0b
Alex Tarnawski <atmarnawski@gmail.com>
2026-01-14 14:51:28 -0600

    create new object for state

--- src/utils.ts
+++ src/utils.ts
@@ -15,17 +15,18 @@ const getLastInTable = (tableHead: Table): Person | Table=> {
   return last;
 }
 
-const insertGuest = (guest: Person, tableHead: Table): void => {
+const insertGuest = (guest: Person, tableHead: Table): Table => {
   // size check not done here, should be done before calling
   const last = getLastInTable(tableHead);
   last.next = guest;
   tableHead.seatsOccupied++;
   guest.table = tableHead.index;
   guest.seat = tableHead.seatsOccupied;
+  return {...tableHead}; // is this necessary?
 }
 
-const removePersonFromTable = () : void => {
-
+const removePersonFromTable = (tableHead: Table) : Table => {
+  return {...tableHead};
 } 
 
 export { createTable, insertGuest, removePersonFromTable, getLastInTable};
