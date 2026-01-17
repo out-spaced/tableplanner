2630be6d8a2a4483f268c47f5b76ea3b6baa7c7c
Alex Tarnawski <amtarnawski@gmail.com>
2026-01-17 14:23:00 -0600

    seat h

--- src/Seat.tsx
+++ src/Seat.tsx
@@ -12,7 +12,7 @@ function Seat({ guest }: { guest: Person }) {
       draggable="true"
       onDragStart={handleDragStart}
       className={`
-        text-xs h-[32px]
+        text-xs h-8
         ${guest.paid ? "bg-green-300" : "bg-red-300"}
         border rounded-md flex justify-center items-center text-center`}
     >
