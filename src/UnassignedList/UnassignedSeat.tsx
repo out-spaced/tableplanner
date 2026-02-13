389b652366af25d7a886b8a9fa2d926a94bf4d55
Alex Tarnawski <amtarnawski@gmail.com>
2026-02-13 11:45:25 -0600

    disappear when draggin

--- src/UnassignedList/UnassignedSeat.tsx
+++ src/UnassignedList/UnassignedSeat.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+
 function UnassignedSeat({ guest }: { guest: Person }) {
+  const [isDragging, setIsDragging] = useState(false);
+
   const handleDragStart = (e: React.DragEvent<HTMLLIElement>) => {
+    setIsDragging(true);
     e.dataTransfer.setData(
       "text",
       JSON.stringify({ index: guest.index, table: 0 }),
@@ -8,15 +13,18 @@ function UnassignedSeat({ guest }: { guest: Person }) {
   };
 
   return (
-    <li
-      draggable="true"
-      onDragStart={handleDragStart}
-      className={`
-        ${guest.paid ? "bg-green-300" : "bg-red-300"}
-        text-sm p-1 mb-1 mt-2 rounded-sm select-none touch-none hover:shadow-md shadow-gray-500`}
-    >
-      {guest.name}
-    </li>
+    <div className={`${isDragging ? "opacity-0" : ""}`}>
+      <li
+        draggable="true"
+        onDragStart={handleDragStart}
+        onDragEnd={() => setIsDragging(false)}
+        className={`
+          ${guest.paid ? "bg-green-300" : "bg-red-300"}
+          text-sm p-1 mb-1 mt-2 rounded-sm select-none touch-none hover:shadow-md shadow-gray-500`}
+      >
+        {guest.name}
+      </li>
+    </div>
   );
 }
 
