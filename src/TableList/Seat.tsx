1d00bf4a8594d011d4a533c1937f8f421bc86f92
Alex Tarnawski <amtarnawski@gmail.com>
2026-02-13 11:48:27 -0600

    disappear from table when dragging

--- src/TableList/Seat.tsx
+++ src/TableList/Seat.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
+
 function Seat({ guest }: { guest: Person }) {
+  const [dragging, setDragging] = useState(false);
   const handleDragStart = (e: React.DragEvent<HTMLLIElement>) => {
+    setDragging(true);
     e.dataTransfer.setData(
       "text",
       JSON.stringify({ index: guest.index, table: guest.table }),
@@ -8,16 +12,19 @@ function Seat({ guest }: { guest: Person }) {
   };
 
   return (
-    <li
-      draggable="true"
-      onDragStart={handleDragStart}
-      className={`
+    <div className={`${dragging ? "opacity-0" : ""}`}>
+      <li
+        draggable="true"
+        onDragStart={handleDragStart}
+        onDragEnd={() => setDragging(false)}
+        className={`
         text-xs h-8 font-medium select-none touch-none
         ${guest.paid ? "bg-green-300" : "bg-red-300"}
         border rounded-md flex justify-center items-center text-center`}
-    >
-      <div>{guest.name}</div>
-    </li>
+      >
+        <div>{guest.name}</div>
+      </li>
+    </div>
   );
 }
 
