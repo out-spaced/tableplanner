6b762afce13491f5f80a0fd14b7b1bea07094621
Alex Tarnawski <amtarnawski@gmail.com>
2026-02-13 12:01:58 -0600

    some drag styling

--- src/TableList/Seat.tsx
+++ src/TableList/Seat.tsx
@@ -12,18 +12,22 @@ function Seat({ guest }: { guest: Person }) {
   };
 
   return (
-    <div className={`${dragging ? "opacity-0" : ""}`}>
-      <li
-        draggable="true"
-        onDragStart={handleDragStart}
-        onDragEnd={() => setDragging(false)}
-        className={`
-        text-xs h-8 font-medium select-none touch-none
-        ${guest.paid ? "bg-green-300" : "bg-red-300"}
-        border rounded-md flex justify-center items-center text-center`}
-      >
-        <div>{guest.name}</div>
-      </li>
+    <div
+      className={`${dragging ? "border rounded-md -mt-px -mb-px bg-gray-200" : ""}`}
+    >
+      <div className={`box-content ${dragging ? "opacity-0" : ""}`}>
+        <li
+          draggable="true"
+          onDragStart={handleDragStart}
+          onDragEnd={() => setDragging(false)}
+          className={`
+          text-xs h-8 font-medium select-none touch-none
+          ${guest.paid ? "bg-green-300" : "bg-red-300"}
+          border rounded-md flex justify-center items-center text-center`}
+        >
+          <div>{guest.name}</div>
+        </li>
+      </div>
     </div>
   );
 }
